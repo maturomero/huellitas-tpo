6d91f2ed529817ba8afd321edbcbd5b929f73e29
Agustina Romero <maturom14@gmail.com>
2025-10-07 17:37:11 -0300

    Cambios en WebConfig

--- demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
+++ demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
+            .cors(cors->{})
             .authorizeHttpRequests(req -> req
                 .requestMatchers("/error/**").permitAll()
 
